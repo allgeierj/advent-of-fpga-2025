65219f2f3022ddd5e3760a40aa1cf805d102f40f
allgeierj <allgeierj@gmail.com>
2025-12-26 16:08:12 -0600

    Simple BCD brute force solution (fails timing @ 250 MHz)

---
+++ day02/sim/Tb.sv
@@ -0,0 +1,30 @@
+module Tb;
+
+logic Clk;
+logic Error;
+logic Done;
+
+initial begin
+    Clk = 1'b0;
+end
+
+// 250 MHz clock
+always #2 Clk = ~Clk;
+
+Top Top_u 
+(
+    .Clk(Clk),
+    .Error(Error),
+    .Done(Done)
+);
+
+always @(posedge Error or posedge Done)
+begin
+    $display("Error: %b, Done: %b", Error, Done);
+    if(Done) begin
+        $display("Password is: %0d", Top_u.Solver_u.Q.InvalidIdSum);
+    end
+    $finish;
+end
+
+endmodule
