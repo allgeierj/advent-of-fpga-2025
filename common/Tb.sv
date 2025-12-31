c79bcfda4b93b053d6b59299735ca97a760b0c78
allgeierj <allgeierj@gmail.com>
2025-12-31 12:31:22 -0600

    Day 03 part 01 solved

--- common/Tb.sv
+++ common/Tb.sv
@@ -26,6 +26,8 @@ begin
         $display("Password is: %0d", Top_u.Solver_u.Q.Password);
         `elsif DAY02
         $display("Sum of invalid IDs is: %0d", Top_u.Solver_u.Q.InvalidIdSum);
+        `elsif DAY03
+        $display("Total joltage is: %0d", Top_u.Solver_u.Q.TotalJoltage);
         `endif
     end
     $finish;
